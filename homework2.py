726cb26dfe8d1f52d380f378a1fdbc4aa4acefdb
Tyrrik <spidman@yandex.ru>
2024-06-28 21:32:34 +0200

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,6 @@
+Exercise=int(input())
+Time=float(input())
+Cours=input()
+Time_on_one=(Exercise/Time)
+print("Курс: ",Cours,', '+"Всего задач: ", Exercise, ', ' + "Всего затрачено часов: ", Time, ', '
+      + "Среднее время выполнения 1 Задания:", Time_on_one,'.')
