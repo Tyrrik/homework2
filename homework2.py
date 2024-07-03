10290255632583a4f49bb73ea6625fc8dbe01780
Tyrrik <spidman@yandex.ru>
2024-07-03 17:55:01 +0200

    Update homework2.py

--- homework2.py
+++ homework2.py
@@ -1,6 +1,6 @@
-Exercise=int(input())
-Time=float(input())
-Cours=input()
-Time_on_one=str((Exercise/Time)/60)
+Exercise=12
+Time=1.5
+Cours='Python'
+Time_on_one=str(Time/Exercise)
 print("Курс: ",Cours,', '+"Всего задач: ", Exercise, ', ' + "Всего затрачено часов: ", Time, ', '
       + "Среднее время выполнения 1 Задания:", Time_on_one[:5], "часа"+'.')
